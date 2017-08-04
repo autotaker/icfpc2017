ac542198577a1f6194eb4adcd6475127b3c14064
cympfh <cympfh@gmail.com>
2017-08-04 23:48:49 +0900

    test

---
+++ cympfh/main.py
@@ -0,0 +1,100 @@
+import sys
+import json
+
+
+def read_json():
+    s = ''
+    while True:
+        t = sys.stdin.read(1)
+        if t == ':':
+            break
+        s += t
+    n = int(s)
+    s = sys.stdin.read(n)
+    return json.loads(s)
+
+
+def write_json(obj):
+    s = json.dumps(obj)
+    print("{}:{}".format(len(s), s))
+
+
+def move_claim(s, t, p):
+    write_json({'claim': {'punter': p, 'source': s, 'target': t}})
+
+
+def move_pass(p):
+    write_json({'pass': {'punter': p}})
+
+
+## Setup
+game = read_json()
+p = game['punter']
+n = game['punters']
+num_vertex = len(game['map']['sites'])
+
+R = len(game['map']['rivers'])
+edges = {}
+neigh = [[] for _ in range(num_vertex)]
+for i, river in enumerate(game['map']['rivers']):
+    river['id'] = i
+    s = river['source']
+    t = river['target']
+    edges[s, t] = river
+    neigh[s].append(river)
+    neigh[t].append(river)
+
+owner = [None] * R
+my_vertexs = []  # [id]
+
+## ready
+write_json({'ready': p, 'state': 'ok'})
+
+## Gameplay
+while True:
+    moves = read_json()['move']['moves']
+    for move in moves:
+        if 'claim' in move:
+            claim = move['claim']
+            s = claim['source']
+            t = claim['target']
+            owner[edge[s, t]['id']] = claim['punter']
+        else:
+            pass
+
+    done = False
+
+    for mine in game['map']['mines']:
+        for edge in neigh[mine]:
+            s = edge['source']
+            t = edge['target']
+            if owner[ edges[s, t]['id'] ] is None:
+                move_claim(s, t, p)
+                my_vertexs.append(s)
+                my_vertexs.append(t)
+                owner[edges[s, t]['id']] = p
+                done = True
+                break
+            if done:
+                break
+        if done:
+            break
+
+    if done:
+        continue
+
+    for s in my_vertexs:
+        for edge in neigh[s]:
+            s = edge.source
+            t = edge.target
+            if owner[ edges[s, t]['id'] ] is not None:
+                continue
+            move_claim(s, t, p)
+            my_vertexs.append(s)
+            my_vertexs.append(t)
+            owner[edges[s, t]['id']] = p
+            done = True
+            break
+
+    if not done:
+        move_pass(p)
