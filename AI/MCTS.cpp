e90b4665cc5a4768867946b13147fc166a0df81b
shuichi <Shuichi Hirahara>
2017-08-05 08:34:33 +0900

    totyu

--- AI/MCTS.cpp
+++ AI/MCTS.cpp
@@ -17,24 +17,11 @@
 
 using namespace std;
 
-
-class MCTS : public Game {
-	Json::Value setup() const override;
-	tuple<int, int, Json::Value> move() const override;
-	void run_simulation();
-};
-
-
-Json::Value MCTS::setup() const {
-	return Json::Value();
-}
-
 typedef pair<int, int> move_t;
 struct Node {
 	int n_plays, n_wins; /* number of plays, wins */
 	int from, to; /* river */
 	int cur_player; /* who's turn? */
-
 	
 	map<move_t, unique_ptr<Node>> children;
 
@@ -42,28 +29,53 @@ struct Node {
 };
 
 
-void MCTS::run_simulation() {
+class MCTS_AI : public Game {
+	Json::Value setup() const override;
+	tuple<int, int, Json::Value> move() const override;
+
+	struct MCTS_Core {
+		MCTS_AI &parent;
+		MCTS_Core(MCTS_AI &parent) : parent(parent), root(-1) {}
+		void run_simulation();
+		pair<int, int> get_play();
+		Node root;
+	};
+	MCTS_Core core;
+public:
+	MCTS_AI() : core(*this) {}
+};
+
+
+Json::Value MCTS_AI::setup() const {
+	return Json::Value();
+}
+
+pair<int, int> MCTS_AI::MCTS_Core::get_play() {
+	return make_pair(0, 1);
+}
+
+void MCTS_AI::MCTS_Core::run_simulation() {
 	/* remaining_turns */
 	int total_edges = 0;
-	for (int i = 0; i < (int)graph.rivers.size(); ++i) {
-		for (const auto& r : graph.rivers[i]) {
+	for (int i = 0; i < (int)parent.graph.rivers.size(); ++i) {
+		for (const auto& r : parent.graph.rivers[i]) {
 			if (i < r.to) total_edges += 1;
 		}
 	}
-	int remaining_turns = total_edges - (int)this->history.size();
+	int remaining_turns = total_edges - (int)parent.history.size();
 
-	Node cur_node(-1);
+	Node *cur_node = &root;
 
-	Graph cur_state = this->graph;
+	Graph cur_state = parent.graph;
 
 	set<int> visited;
 	bool expanded = false;
-	int cur_player = this->punter_id;
+	int cur_player = parent.punter_id;
 
 	vector<Node*> visited_nodes;
-	visited_nodes.push_back(&cur_node);
+	visited_nodes.push_back(cur_node);
 	while(--remaining_turns >= 0) {
-		int next_player = (cur_player + 1) % this->num_punters;
+		int next_player = (cur_player + 1) % parent.num_punters;
 
 		/* get next legal moves */
 		vector<move_t> legal_moves;
@@ -75,10 +87,10 @@ void MCTS::run_simulation() {
 
 		move_t move = legal_moves[rand() % legal_moves.size()];
 
-		if (!expanded && cur_node.children.count(move) == 0) {
+		if (!expanded && cur_node->children.count(move) == 0) {
 			/* expand node */
 			expanded = true;
-			cur_node.children[move] = unique_ptr<Node>(new Node(cur_player));
+			cur_node->children[move] = unique_ptr<Node>(new Node(cur_player));
 		}
 		/* apply "move" to "cur_state" */
 		for(auto& r : cur_state.rivers[move.first]) {
@@ -94,66 +106,43 @@ void MCTS::run_simulation() {
 			}
 		}
 
-		/*
-		if (cur_node.children.count(move)) {
-			cur_node = cur_node.children[move]
-			visited_nodes.push_back(cur_node.
-				[cur_node.children[move]]
+		if (cur_node->children.count(move)) {
+			cur_node = &(*cur_node->children[move]);
+			visited_nodes.push_back(cur_node);
 		}
-		*/
-
-		visited_nodes.push_back(&cur_node);
 
 		cur_player = next_player;
 	}
+
+	/* determine winner of this playout */
+	set<int> winners;
+	vector<int64_t> scores = cur_state.evaluate(parent.num_punters);
+	vector<pair<int64_t, int>> scores2;
+	for(int i=0; i<(int)scores.size(); i++) {
+		scores2.emplace_back(scores[i], i);
+	}
+	sort(scores2.rbegin(), scores2.rend());
+	for(int i=0; i<(int)scores2.size(); i++) {
+		if (i>0 && scores2[i-1].first != scores2[i].first) break; /* include all ties */
+		winners.insert(i);
+	}
+
+
+	/* back propagate */
+	for(auto p : visited_nodes) {
+		p->n_plays += 1;
+		if (winners.count(p->cur_player)) p->n_wins += 1;
+	}
 }
 
-tuple<int, int, Json::Value> MCTS::move() const {
-
-
-	/*
-	srand(time(NULL));
-
-	UnionFind uf(graph.num_vertices);
-	std::vector<pair<int, int> > no_edge;
-	std::vector<pair<int, int> > yes_edge;
-
-	for (size_t i = 0; i < graph.rivers.size(); ++i) {
-		for (const auto& r : graph.rivers[i]) {
-			if (r.punter == punter_id) {
-				uf.unit(i, r.to);
-			}
-		}
-	}
-
-	for (size_t i = 0; i < graph.rivers.size(); ++i) {
-		for (const auto& r : graph.rivers[i]) {
-			if (r.punter != -1) continue;
-
-			if (uf.find(i) == uf.find(r.to)) {
-				no_edge.emplace_back(i, r.to);
-			} else {
-				yes_edge.emplace_back(i, r.to);
-			}
-		}
-	}
-
-	std::pair<int, int> e;
-
-	if (yes_edge.empty()) {
-		e = no_edge[rand() % no_edge.size()];
-	} else {
-		e = yes_edge[rand() % yes_edge.size()];
-	}
-
-	return make_tuple(e.first, e.second, Json::Value());
-	*/
+tuple<int, int, Json::Value> MCTS_AI::move() const {
+	core.get_play();
 	return make_tuple(0, 1, Json::Value());
 }
 
 int main()
 {
-	MCTS ai;
+	MCTS_AI ai;
 	ai.run();
 	return 0;
 }
