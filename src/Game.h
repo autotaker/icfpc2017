549bb876e214e9bada1d89952ac385e09312b5e2
japlj <ys.qicos@gmail.com>
2017-08-04 23:15:02 +0900

    Add Game.(cpp|h)

---
+++ src/Game.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <vector>
+#include "../jsoncpp/json/json.h"
+
+struct Graph {
+  struct River {
+    int to;
+    int punter;
+    River(int to, int punter = -1);
+  };
+  int num_mines;
+  int num_vertices;
+  std::vector<std::vector<River>> rivers;
+};
+
+struct Move {
+  int punter;
+  int src;
+  int to;
+  Move(int punter, int src, int to);
+
+  bool is_pass() const;
+};
+using History = std::vector<Move>;
+
+class Game {
+private:
+  int num_punters;
+  int punter_id;
+  Graph graph;
+  History history;
+  Json::Value info;
+
+  Json::Value encode_state(Json::Value info) const;
+
+public:
+  virtual Json::Value setup() const = 0;
+  virtual std::tuple<int, int, Json::Value> move() const = 0;
+
+  void run();
+};
