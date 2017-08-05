0866d56eceb0ddd32da83d559d1fe966b412f6da
Keiichi Watanabe <udon.watanabe@gmail.com>
2017-08-05 16:10:11 +0900

    add future settings in server/main.py

--- server/game.py
+++ server/game.py
@@ -8,7 +8,11 @@ class Game:
         for river in self.game['rivers']:
             river['owned_by'] = -1
         self.state = [ None for i in range(n) ]
-    
+
+        # For optional rules
+        self.settings = {"futures": True}
+        self.futures = [ [] for i in range(n) ]
+
     def move_claim(self, player_id, source, target):
         for river in self.game['rivers']:
             if (river['source'] == source and river['target'] == target or
@@ -19,5 +23,3 @@ class Game:
                 else:
                     return "the river is owned by other player"
         return "no such river"
-
-
