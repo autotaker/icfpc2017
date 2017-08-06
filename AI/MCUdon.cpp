2432967d426e776a9ada2c92780b26eff2889532
japlj <ys.qicos@gmail.com>
2017-08-07 02:50:01 +0900

    piyoyo-

---
+++ AI/MCUdon.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+#include "Game.h"
+#include "json/json.h"
+
+#define _USE_AS_ENGINE
+#include "SuUdon.cpp"
+#include "MCTS_Greedy.cpp"
+#undef _USE_AS_ENGINE
+
+using namespace std;
+
+static const char* MODE = "mode";
+static const char* ORG_INFO = "org_info";
+
+class MCUdon : public Game {
+  SetupSettings setup() const override;
+  MoveResult move() const override;
+  std::string name() const override;
+
+  template<typename T> MoveResult trymove() const;
+};
+
+std::string
+MCUdon::name() const {
+  return "MC_Udon";
+}
+
+SetupSettings
+MCUdon::setup() const {
+  SuUdonAI::SuUdonAI udon;
+  udon.import(*this);
+  auto res = udon.setup();
+
+  SetupSettings r = res;
+  r.info = Json::Value();
+  r.info[MODE] = 0;
+  r.info[ORG_INFO] = res.info;
+  return r;
+}
+
+template<typename T> MoveResult
+MCUdon::trymove() const {
+  T ai;
+  ai.import(*this);
+  return ai.move();
+}
+
+MoveResult
+MCUdon::move() const {
+  int mode = info[MODE].asInt();
+  MoveResult r((Json::Value()));
+  info_for_import = info[ORG_INFO];
+  while (true) {
+    switch (mode) {
+      case 0: r = trymove<SuUdonAI::SuUdonAI>(); break;
+      case 1: r = trymove<MCTS_greedy::MCTS_GREEDY_AI>(); break;
+    }
+    if (!r.valid) {
+      mode = 1;
+      MCTS_greedy::MCTS_GREEDY_AI ai;
+      ai.import(*this);
+      info_for_import = ai.walkin_setup();
+    } else {
+      break;
+    }
+  }
+
+  MoveResult res = r;
+  res.info = Json::Value();
+  res.info[MODE] = mode;
+  res.info[ORG_INFO] = r.info;
+  return res;
+}
+
+int main() {
+  MCUdon ai;
+  ai.run();
+  return 0;
+}
--- lib/Game.h
+++ lib/Game.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdint>
 #include <string>
+#include <cassert>
 #include "json/json.h"
 
 namespace json_helper {
@@ -77,6 +78,9 @@ struct MoveResult {
   std::vector<int> splurge_path;
   Json::Value info;
 
+  bool valid = true;
+  void done() { valid = false; }
+
   MoveResult(const Json::Value& info);  // pass
   MoveResult(const std::tuple<int, int, Json::Value>& move);  // claim
   MoveResult(const std::vector<int>& path, const Json::Value& info); // splurge
@@ -105,6 +109,8 @@ protected:
 
   void calc_shortest_paths(int src, std::vector<int>& dist, std::vector<int>& path) const;
   void calc_cur_dists(std::vector<std::vector<int>>& dists, std::vector<std::vector<int>>& prevs) const;
+
+  mutable Json::Value info_for_import;
 
 private:
   bool first_turn;
@@ -147,6 +153,10 @@ public:
 
   virtual SetupSettings setup() const = 0;
   virtual MoveResult move() const = 0;
+  virtual Json::Value walkin_setup() const { assert(false); };
+
+  // for meta AI
+  void import(const Game& meta_ai);
 
   virtual std::string name() const { return "AI"; };
 
