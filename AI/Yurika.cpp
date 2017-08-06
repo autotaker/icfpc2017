d6b1b9f6d47f5c69faab784258e448d6c778d6a5
hiroh <hiroh@myhome.hp-debian>
2017-08-06 15:07:07 +0900

    Implement a disturb mechanism in Yurika

--- AI/Yurika.cpp
+++ AI/Yurika.cpp
@@ -26,11 +26,17 @@ struct Data {
   int degree;
 };
 
+struct MyMove {
+  int src;
+  int to;
+  ll score;
+};
+
 class AI : public Game {
   SetupSettings setup() const override;
   tuple<int, int, Json::Value> move() const override;
   std::string name() const override;
-  pair<int,int> getBestData(int pid, const Data& base_data, const vector<int> &in_vertices, Graph& myown_graph) const;
+  MyMove getBestData(int pid, const Data& base_data, const vector<int> &in_vertices, Graph& myown_graph) const;
 };
 
 std::string AI::name() const {
@@ -57,9 +63,11 @@ bool shouldChange(Data* best_data, const Data& current_data) {
 
 #define INF 1070000000LL
 
-pair<int,int> AI::getBestData(int pid, const Data& base_data, const vector<int> &in_vertices, Graph& myown_graph) const {
+MyMove AI::getBestData(int pid, const Data& base_data, const vector<int> &in_vertices, Graph& myown_graph) const {
   Data best_data = base_data;
-  int src = -1, to = -1;
+  MyMove next_move;
+  next_move.src = 0; next_move.to = 0; next_move.score = 0;
+  
   for (int v = 0; v < (int) myown_graph.num_vertices; v++) {
     for (auto& r :  myown_graph.rivers[v]) {
       auto nv = r.to;
@@ -90,14 +98,15 @@ pair<int,int> AI::getBestData(int pid, const Data& base_data, const vector<int> 
       current_data.point = next_point;
       current_data.degree = myown_graph.rivers[nv].size();
       if (shouldChange(&best_data, current_data)) {
-        src = v;
-        to = nv;
+        next_move.src = v;
+        next_move.to = nv;
+        next_move.score = next_point;
       }
       r.punter = -1;
       nrit->punter = -1;
     }
   }
-  return make_pair(src, to);
+  return next_move;
 }
 
 
@@ -107,112 +116,63 @@ tuple<int,int, Json::Value> AI::move() const {
   // try to connect them.
   // tie break is solved by degree.
  
-  auto start_time = chrono::system_clock::now();
+  //auto start_time = chrono::system_clock::now();
   
   std::vector<int> in_vertices(graph.num_vertices, 0);
   for (int i = 0, N = vertices_.size(); i < N; i++) {
     in_vertices[vertices_[i].asInt()] = 1;
   }
   
-  vector<int> degs(graph.num_vertices, 0);
-  for (int i = 0; i < graph.num_vertices; i++) {
-    for (auto &r : graph.rivers[i]) {
-      if (r.punter == -1) {
-        degs[i] += 1;
-      }
-    }
-  }
-
-  vector<tuple<int,int,int>> mine_results[2];
-  for (int i = 0; i < graph.num_mines; i++) {
-    for (auto &r : graph.rivers[i]) {
-      if (r.punter != -1) {
-        continue;
-      }
-      int to = r.to;
-      mine_results[in_vertices[i]].emplace_back(degs[to], i, to);
-    }
-  }
-
-  if (mine_results[0].size() > 0) {
-    Json::Value ret_vertices_ = vertices_;
-    auto pick = *max_element(mine_results[0].begin(), mine_results[0].end());
-    auto src = std::get<1>(pick);
-    auto to = std::get<2>(pick);
-    if (!in_vertices[src]) {
-      ret_vertices_.append(src);
-    }
-    if (!in_vertices[to]) {
-      ret_vertices_.append(to);
-    }
-    return make_tuple(src, to, ret_vertices_);
-  } else if (mine_results[1].size() > 0){
-    Json::Value ret_vertices_ = vertices_;
-    auto pick = *max_element(mine_results[1].begin(), mine_results[1].end());
-    auto src = std::get<1>(pick);
-    auto to = std::get<2>(pick);
-    if (!in_vertices[src]) {
-      ret_vertices_.append(src);
-    }
-    if (!in_vertices[to]) {
-      ret_vertices_.append(to);
-    }
-    return make_tuple(src, to, ret_vertices_);
-  }
-  
-
-  
   Graph myown_graph = graph;
-  const auto& points = myown_graph.evaluate(num_punters, shortest_distances);
-  Data base_data;
-  base_data.point = points[punter_id];
-  base_data.degree = INF;
+  const auto& scores = myown_graph.evaluate(num_punters, shortest_distances);
+  
+  std::vector<ll> increase_scores(num_punters);
+  std::vector<MyMove> next_move(num_punters);
+  for (int i = 0; i < num_punters; i++) {
+    Data base_data;
+    base_data.point = scores[i];
+    base_data.degree = INF;
+    next_move[i]  = getBestData(i, base_data, in_vertices, myown_graph);
+    increase_scores[i] = next_move[i].score - scores[i];
+  }
 
-  // First, try to maximize my score
-  auto src_to = getBestData(punter_id, base_data, in_vertices, myown_graph);
-  //cerr << src_to.first << " " << src_to.second << " maximize me" << endl;
-  if (src_to.first == -1 && src_to.second == -1) {
-    // Fail. That is, there is no edge to increase my score.
-    // Then, try to decrease the final another's score.
-    vector<pair<ll, int>> enemys;
-    for (int i = 0; i < num_punters; i++) {
-      if (i == punter_id) {
-        continue;
-      }
-      enemys.emplace_back(points[i], i);
+  ll myscore = increase_scores[punter_id];
+  ll best_score = 0;
+  int target_punter = -1;
+  for (int i = 0; i < num_punters; i++) {
+    if (i == punter_id) {
+      continue;
     }
-    sort(enemys.begin(), enemys.end());
-    reverse(enemys.begin(), enemys.end());
-    Data enemy_base_data;
-    for (const auto&  e : enemys) {
-      enemy_base_data.point = e.first;
-      enemy_base_data.degree = INF;
-      const auto& enemy_src_to = getBestData(e.second, enemy_base_data, in_vertices, myown_graph);
-      if (enemy_src_to.first != -1 && enemy_src_to.second != -1) {
-        src_to = enemy_src_to;
-        break;
-      }
-      auto elapsed_time = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start_time).count();
-      if (elapsed_time >= 900) {
-        break;
-      }
+    if (best_score < increase_scores[i]) {
+      best_score = increase_scores[i];
+      target_punter = i;
     }
   }
-  int src = src_to.first;
-  int to = src_to.second;
-  
-  if (src == -1 && to == -1) {
-    // In this case, I pick up any vertex.
-    for (int v = 0; v < (int) myown_graph.num_vertices; v++) {
-      for (const auto& r :  myown_graph.rivers[v]) {
-        if (r.punter == -1) {
-          src = v;
-          to = r.to;
-          goto out;
+  int src = -1;
+  int to = -1;
+  if (target_punter == -1) {
+    if (myscore > 0) {
+    src = next_move[punter_id].src;
+    to = next_move[punter_id].to;
+    } else {
+      // Nobody can get point.
+      // Then, get any edge
+      // TODO(hiroh): get the edge around another player
+      for (int i = 0; i < graph.num_vertices; i++) {
+        for (const auto& r : graph.rivers[i]) {
+          if (r.punter == -1) {
+            src = i;
+            to = r.to;
+            goto out;
+          }
         }
       }
     }
+  } else if (best_score > myscore * (num_punters)){
+    src = next_move[target_punter].src;
+    to = next_move[target_punter].to;
   }
+  
  out:
   assert(src != -1 && to != -1);
   Json::Value ret_vertices_ = vertices_;
