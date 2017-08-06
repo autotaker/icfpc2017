c823cdeb45fb5a09155a4ec2b6622a706bf7dca1
autotaker <autotaker@gmail.com>
2017-08-06 09:55:52 +0900

    update flask server

--- flask/schema.sql
+++ flask/schema.sql
@@ -16,24 +16,22 @@ CREATE TABLE IF NOT EXISTS AI (
     name varchar(16),
     small_rating real,
     med_rating real,
-    large_rating real);
+    large_rating real,
+    status varchar(16)
+    );
 
 CREATE TABLE IF NOT EXISTS game (
     key integer PRIMARY KEY AUTOINCREMENT,
     id varchar(16) UNIQUE,
-    map_id integer,
+    map_key integer,
     status varchar(16),
     created_at TIMESTAMP DEFAULT (DATETIME('now', 'localtime')));
 
 CREATE TABLE IF NOT EXISTS game_match (
-    game_id integer,
-    ai_id integer,
-    play_order integer);
-
-CREATE TABLE IF NOT EXISTS game_result (
-    game_id integer,
-    ai_id integer,
+    game_key integer,
+    ai_key integer,
+    play_order integer,
     score integer,
-    rank integer);
+    rank integer
+    );
 
-    
