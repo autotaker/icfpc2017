99519b2bfe042e02f860b3b8a3bdab2f9024cf2f
autotaker <autotaker@gmail.com>
2017-08-06 13:08:44 +0900

    change test.sh

--- flask/test.sh
+++ flask/test.sh
@@ -1,8 +1,11 @@
-virtualenv env
+if [ ! -e env ] ;  then
+    virtualenv env
+fi
 source env/bin/activate
 pip install -r requirements.txt
 python register_maps.py
 sqlite3 db.sqlite3 < schema.sql
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
-flask run
+python main.py
+#flask run
